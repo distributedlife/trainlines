931916b88f23eae340655f115c507fcd3d7cf6eb
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-07-27 11:59:27 +1000

    performance tweaks to reduce the db time

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-require 'geokit'
+#require 'geokit'
 
 # Initialize the rails application
 Trainmap::Application.initialize!
